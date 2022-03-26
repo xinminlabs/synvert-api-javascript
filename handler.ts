7a03edd998b24531eda2829d9feb06ea512eba93
Richard Huang <flyerhzm@gmail.com>
2022-03-26 21:13:49 +0800

    update const examples

--- handler.ts
+++ handler.ts
@@ -6,34 +6,48 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 export const generateAst = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  const requestBody = JSON.parse(event.body);
-  const source = requestBody.code;
-  const options = {
-    ecmaVersion: "latest",
-    loc: true,
-    sourceType: "module",
-  };
-  const node = espree.parse(source, options);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ node })
+  try {
+    const requestBody = JSON.parse(event.body);
+    const source = requestBody.code;
+    const options = {
+      ecmaVersion: "latest",
+      loc: true,
+      sourceType: "module",
+    };
+    const node = espree.parse(source, options);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ node })
+    }
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message })
+    }
   }
 }
 
 export const parseSynvertSnippet = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  const requestBody = JSON.parse(event.body);
-  const path = requestBody.jsx ? 'code.jsx' : 'code.js';
-  const input = requestBody.code;
-  const snippet = requestBody.snippet;
-  const rewriter = eval(snippet);
-  mock({ [path]: input });
-  rewriter.process();
-  const output = fs.readFileSync(path, 'utf-8');
-  mock.restore();
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ output })
+  try {
+    const requestBody = JSON.parse(event.body);
+    const path = requestBody.jsx ? 'code.jsx' : 'code.js';
+    const input = requestBody.code;
+    const snippet = requestBody.snippet;
+    const rewriter = eval(snippet);
+    mock({ [path]: input });
+    rewriter.process();
+    const output = fs.readFileSync(path, 'utf-8');
+    mock.restore();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ output })
+    }
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message })
+    }
   }
 }
