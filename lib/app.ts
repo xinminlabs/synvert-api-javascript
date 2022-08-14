61fa4178c4914fedf3912668aedc22043e421603
Richard Huang <flyerhzm@gmail.com>
2022-08-14 17:13:39 +0800

    validate inputs and outputs

--- lib/app.ts
+++ lib/app.ts
@@ -3,12 +3,32 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import morgan from 'morgan';
 import { generateAst, generateSnippet, parseSynvertSnippet } from './api';
+import { parseCode } from "./magic/utils";
 
 const port = process.env.PORT || 3000;
 const app: Express = express();
 const jsonParser = bodyParser.json();
 app.use(cors())
 app.use(morgan('combined'))
+
+const validateInputsOutputs = (req: Request, res: Response, next: NextFunction) => {
+  if (!req.body.inputs || !Array.isArray(req.body.inputs) || req.body.inputs.length === 0) {
+    return res.status(400).json({ error: "Inputs are invalid." });
+  }
+  if (!req.body.outputs || !Array.isArray(req.body.outputs) || req.body.outputs.length === 0) {
+    return res.status(400).json({ error: "Outputs are invalid." });
+  }
+  if (req.body.inputs.length !== req.body.outputs.length) {
+    return res.status(400).json({ error: "Inputs size is not equal to outputs size." });
+  }
+  if (req.body.inputs.some(input => typeof parseCode(req.body.extension, input) === "undefined")) {
+    return res.status(400).json({ error: "Inputs are invalid." });
+  }
+  if (req.body.outputs.some(output => typeof parseCode(req.body.extension, output) === "undefined")) {
+    return res.status(400).json({ error: "Outputs are invalid." });
+  }
+  next();
+}
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Synvert!');
@@ -24,7 +44,7 @@ app.post('/parse-synvert-snippet', jsonParser, (req: Request, res: Response) => 
   res.json({ output });
 });
 
-app.post('/generate-snippet', jsonParser, (req: Request, res: Response) => {
+app.post('/generate-snippet', jsonParser, validateInputsOutputs, (req: Request, res: Response) => {
   const snippet = generateSnippet(req.body.extension, req.body.inputs, req.body.outputs);
   res.json({ snippet });
 });
