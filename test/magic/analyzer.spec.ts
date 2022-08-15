0b7480c284d762aac0ec8f4487cfc94f6934876f
Richard Huang <flyerhzm@gmail.com>
2022-08-15 16:16:27 +0800

    analyze tsx code

--- test/magic/analyzer.spec.ts
+++ test/magic/analyzer.spec.ts
@@ -3,16 +3,38 @@ import Analyzer from "../../lib/magic/analyzer";
 
 describe("Analyzer", () => {
   describe("#call", () => {
-    it("gets pattern", () => {
-      const inputs = ["$.isArray(foo)", "$.isArray(bar)"];
-      const outputs = ["Array.isArray(foo)", "Array.isArray(bar)"];
-      const analyzer = new Analyzer("ts", inputs, outputs);
-      const expected = dedent`
-        withNode({ nodeType: "CallExpression", expression: { nodeType: "PropertyAccessExpression", expression: "$", name: "isArray" }, arguments: { length: 1 } }, () => {
-          replaceWith("Array.{{expression.name}}({{arguments.0}})");
-        });
-      `;
-      expect(analyzer.call()).toEqual([expected]);
+    describe("ts", () => {
+      it("gets pattern", () => {
+        const inputs = ["$.isArray(foo)", "$.isArray(bar)"];
+        const outputs = ["Array.isArray(foo)", "Array.isArray(bar)"];
+        const analyzer = new Analyzer("ts", inputs, outputs);
+        const expected = dedent`
+          withNode({ nodeType: "CallExpression", expression: { nodeType: "PropertyAccessExpression", expression: "$", name: "isArray" }, arguments: { length: 1 } }, () => {
+            replaceWith("Array.{{expression.name}}({{arguments.0}})");
+          });
+        `;
+        expect(analyzer.call()).toEqual([expected]);
+      });
+    });
+
+    describe("tsx", () => {
+      it("gets pattern", () => {
+        const inputs = [dedent`
+          <div className="container-fluid">
+          </div>
+        `];
+        const outputs = [dedent`
+          <Container fluid>
+          </Container>
+        `];
+        const analyzer = new Analyzer("tsx", inputs, outputs);
+        const expected = dedent`
+          withNode({ nodeType: "JsxElement", openingElement: { nodeType: "JsxOpeningElement", tagName: "div", attributes: { nodeType: "JsxAttributes", properties: { 0: { nodeType: "JsxAttribute", name: " className", initializer: { nodeType: "StringLiteral", text: "container-fluid", singleQuote: undefined } }, length: 1 } } }, children: { 0: { nodeType: "JsxText", text: "\n" }, length: 1 }, closingElement: { nodeType: "JsxClosingElement", tagName: "div" } }, () => {
+            replaceWith("<Container fluid>{{children.0}}</Container>");
+          });
+        `;
+        expect(analyzer.call()).toEqual([expected]);
+      });
     });
   });
 });
