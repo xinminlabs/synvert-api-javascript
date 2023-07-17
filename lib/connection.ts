6e8309e02fc0e7309981ce771573c47c9b7a9b55
Richard Huang <flyerhzm@gmail.com>
2023-07-18 07:34:43 +0800

    connect db and redis when used

--- lib/connection.ts
+++ lib/connection.ts
@@ -1,9 +1,6 @@
 import { Sequelize } from 'sequelize';
 import { createClient } from 'redis';
 
-const DATABASE_CLIENT = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres@localhost/synvert_development');
-const REDIS_CLIENT = createClient({ url: process.env.REDIS_URL || 'redis://localhost:6379' });
+export const databaseClient = () => new Sequelize(process.env.DATABASE_URL || 'postgres://postgres@localhost/synvert_development');
 
-export const databaseClient = () => DATABASE_CLIENT;
-
-export const redisClient = () => REDIS_CLIENT;
+export const redisClient = () => createClient({ url: process.env.REDIS_URL || 'redis://localhost:6379' });
