f24dd8ab8b121065ce949186af09d1ac7cfef243
Richard Huang <flyerhzm@gmail.com>
2023-07-18 07:44:38 +0800

    log connection

--- lib/connection.ts
+++ lib/connection.ts
@@ -1,6 +1,12 @@
 import { Sequelize } from 'sequelize';
 import { createClient } from 'redis';
 
-export const databaseClient = () => new Sequelize(process.env.DATABASE_URL || 'postgres://postgres@localhost/synvert_development');
+export const databaseClient = () => {
+  console.log(process.env.DATABASE_URL || 'postgres://postgres@localhost/synvert_development');
+  return new Sequelize(process.env.DATABASE_URL || 'postgres://postgres@localhost/synvert_development');
+}
 
-export const redisClient = () => createClient({ url: process.env.REDIS_URL || 'redis://localhost:6379' });
+export const redisClient = () => {
+  console.log({ url: process.env.REDIS_URL || 'redis://localhost:6379' });
+  return createClient({ url: process.env.REDIS_URL || 'redis://localhost:6379' });
+}
